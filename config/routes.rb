2c75cfcd4646b8c70dc1a3633b837a0bd1f3acf0
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2025-05-17 14:06:09 -0400

    feat: nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects
+  resources :projects do
+    resources :tasks
+  end
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
